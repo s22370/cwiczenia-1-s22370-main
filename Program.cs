b63d612430414e6c684c2cfd80c81d4697d9ccd8
Kamil <s22370@pjwstk.edu.pl>
2022-03-15 23:42:47 +0100

    accepted

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cwiczenia_1_s22370_main
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
